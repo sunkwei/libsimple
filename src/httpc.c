15169d595ada0c6bea636f692e07e36b9a53bdf6
sunkw <sunkwei@gmail.com>
2013-12-18 17:25:36 +0800

    a http msg parser

---
+++ src/httpc.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "../include/simple/httpparser.h"
+
+#ifdef WIN32
+#define strcasecmp stricmp
+#define snprintf _snprintf
+#endif // win32
+
+#define safefree(x) {	\
+	if (x) free(x);	\
+	x = 0;		\
+}
+
+HttpMessage *httpc_Message_create ()
+{
+	HttpMessage *msg = (HttpMessage *)malloc(sizeof(HttpMessage));
+
+	msg->StartLine.p1 = 0;
+	msg->StartLine.p2 = 0;
+	msg->StartLine.p3 = 0;
+
+	msg->pri_data = 0;
+
+	msg->headers = 0;
+	msg->header_cnt = 0;
+
+	msg->body = 0;
+	msg->bodylen = 0;
+
+	return msg;
+}
+
+extern void httpc_internal_releaseParserData (void *pridata);
+
+void httpc_Message_release (HttpMessage *msg)
+{
+	int i;
+	if (msg) {
+		safefree(msg->StartLine.p1);
+		safefree(msg->StartLine.p2);
+		safefree(msg->StartLine.p3);
+
+		if (msg->headers) {
+			for (i = 0; i < msg->header_cnt; i++) {
+				safefree(msg->headers[i].key);
+				safefree(msg->headers[i].value);
+			}
+			safefree(msg->headers);
+		}
+		msg->header_cnt = 0;
+
+		safefree(msg->body);
+		msg->bodylen = 0;
+
+		httpc_internal_releaseParserData(msg->pri_data);
+		msg->pri_data = 0;
+	}
+	free(msg);
+}
+
+void httpc_Message_setStartLine (HttpMessage *msg,
+		const char *p1, const char *p2, const char *p3)
+{
+	if (msg && p1 && p2 && p3) {
+		safefree(msg->StartLine.p1);
+		msg->StartLine.p1 = strdup(p1);
+
+		safefree(msg->StartLine.p2);
+		msg->StartLine.p2 = strdup(p2);
+
+		safefree(msg->StartLine.p3);
+		msg->StartLine.p3 = strdup(p3);
+	}
+}
+
+int httpc_Message_setValue (HttpMessage *msg,
+		const char *key, const char *value)
+{
+	int i;
+	if (!msg || !key || !value) return -1;
+	for (i = 0; i < msg->header_cnt; i++) {
+		if (!strcasecmp(key, msg->headers[i].key)) {
+			safefree(msg->headers[i].value);
+			msg->headers[i].value = strdup(value);
+
+			return 1;
+		}
+	}
+
+	msg->headers = (HttpHeader *)realloc(msg->headers,
+			(msg->header_cnt+1) * sizeof(HttpHeader));
+	msg->headers[msg->header_cnt].key = strdup(key);
+	msg->headers[msg->header_cnt].value = strdup(value);
+
+	msg->header_cnt++;
+
+	return 0;
+}
+
+int httpc_Message_getValue (HttpMessage *msg,
+		const char *key, const char **value)
+{
+	int i;
+	if (!msg || !key) return -1;
+	for (i = 0; i < msg->header_cnt; i++) {
+		if (!strcasecmp(key, msg->headers[i].key)) {
+			*value = msg->headers[i].value;
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int httpc_Message_delValue (HttpMessage *msg,
+		const char *key)
+{
+	// TODO: 优化的处理管理内存
+	assert(0);
+	return -1;
+}
+
+int httpc_Message_appendBody (HttpMessage *msg,
+		const char *data, int len)
+{
+	char tmp[16];
+	if (!msg) return -1;
+	if (len == 0) return msg->bodylen;
+	msg->body = (char*)realloc(msg->body, msg->bodylen+len);
+	memcpy(&msg->body[msg->bodylen], data, len);
+	msg->bodylen += len;
+
+	snprintf(tmp, sizeof(tmp), "%u", msg->bodylen);
+	httpc_Message_setValue(msg, "Content-Length", tmp);
+
+	return msg->bodylen;
+}
+
+void httpc_Message_clearBody (HttpMessage *msg)
+{
+	if (msg) {
+		safefree(msg->body);
+		msg->bodylen = 0;
+	}
+}
+
+int httpc_Message_getBody (HttpMessage *msg,
+		const char **data)
+{
+	if (!msg) return -1;
+	*data = msg->body;
+	return msg->bodylen;
+}
+
+int httpc_Message_getLength (HttpMessage *msg)
+{
+	int len = 0, i;
+	if (!msg || !msg->StartLine.p1 || !msg->StartLine.p2 || !msg->StartLine.p3) return -1;
+
+	len += strlen(msg->StartLine.p1) + 1; // ' '
+	len += strlen(msg->StartLine.p2) + 1; // ' '
+	len += strlen(msg->StartLine.p3);
+	len += 2;	// \r\n
+
+	for (i = 0; i < msg->header_cnt; i++) {
+		len += strlen(msg->headers[i].key) + 2; // ": "
+		len += strlen(msg->headers[i].value);
+		len += 2;	// \r\n
+	}
+
+	len += 2;	// \r\n
+	len += msg->bodylen;
+
+	return len;
+}
+
+void httpc_Message_encode (HttpMessage *msg, char *buf)
+{
+	int i;
+	if (msg) {
+		char *p = buf;
+
+		strcpy(p, msg->StartLine.p1);
+		p += strlen(msg->StartLine.p1);
+		strcpy(p, " ");
+		p += 1;
+		strcpy(p, msg->StartLine.p2);
+		p += strlen(msg->StartLine.p2);
+		strcpy(p, " ");
+		p += 1;
+		strcpy(p, msg->StartLine.p3);
+		p += strlen(msg->StartLine.p3);
+		strcpy(p, "\r\n");
+		p += 2;
+
+		for (i = 0; i < msg->header_cnt; i++) {
+			strcpy(p, msg->headers[i].key);
+			p += strlen(msg->headers[i].key);
+			strcpy(p, ": ");
+			p += 2;
+			strcpy(p, msg->headers[i].value);
+			p += strlen(msg->headers[i].value);
+			strcpy(p, "\r\n");
+			p += 2;
+		}
+
+		strcpy(p, "\r\n");
+		p += 2;
+
+		memcpy(p, msg->body, msg->bodylen);
+	}
+}
---
+++ src/httpparser.c
@@ -0,0 +1,321 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "../include/simple/httpparser.h"
+
+#define EXPSIZE 128
+
+#define safefree(x) {	\
+	if (x) free(x);	\
+	x = 0;		\
+}
+
+// private data for parser
+struct ParserData
+{
+	char *linedata;
+	int line_bufsize;
+	int line_datasize;
+
+	enum HttpParserState state, last_state;
+};
+typedef struct ParserData ParserData;
+
+void httpc_internal_releaseParserData (void *p)
+{
+	ParserData *data = (ParserData *)p;
+	if (!p) return;
+
+	if (data->linedata) {
+		free(data->linedata);
+	}
+
+	free(data);
+}
+
+static int isblank (int c)
+{
+	if (c == ' ' || c == '\t')
+		return 1;
+	else
+		return 0;
+}
+
+static int isspace (int c)
+{
+	if (isblank(c) || c == '\r' || c == '\n')
+		return 1;
+	else
+		return 0;
+}
+
+static void _append (char c, HttpMessage *msg)
+{
+	ParserData *pd = (ParserData *)msg->pri_data;
+	if (pd->line_datasize + 1 > pd->line_bufsize) {
+		pd->line_bufsize += EXPSIZE;
+		pd->linedata = (char *)realloc(pd->linedata, pd->line_bufsize);
+	}
+	pd->linedata[pd->line_datasize++] = c;
+}
+
+static int _getmsglen (HttpMessage *msg)
+{
+	const char *v = 0;
+	if (httpc_Message_getValue(msg, "Content-Length", &v)) {
+		return atoi(v);
+	}
+	else {
+		return 0;
+	}
+}
+
+static int parseStartLine (HttpMessage *msg)
+{
+	ParserData *pd = (ParserData *)msg->pri_data;
+	const char *p = pd->linedata;
+	const char *tail = pd->linedata + pd->line_datasize;
+	int i;
+
+	// method/version + url/code + version/status
+	char *tmp = (char *)malloc(pd->line_datasize+1);
+
+	// p1
+	memset(tmp, 0, pd->line_datasize);
+	for (i = 0; p < tail && !isspace(*p); p++, i++) {
+		tmp[i] = *p;
+	}
+	safefree(msg->StartLine.p1);
+	msg->StartLine.p1 = strdup(tmp);
+
+	while (p < tail && isspace(*p)) p++;
+
+	// p2
+	memset(tmp, 0, pd->line_datasize);
+	for (i = 0; p < tail && !isspace(*p); p++, i++) {
+		tmp[i] = *p;
+	}
+	safefree(msg->StartLine.p2);
+	msg->StartLine.p2 = strdup(tmp);
+
+	while (p < tail && isspace(*p)) p++;
+
+	// p3
+	memset(tmp, 0, pd->line_datasize);
+	for (i = 0; p < tail && *p != '\r' && *p != '\n'; p++, i++) {
+		tmp[i] = *p;
+	}
+	safefree(msg->StartLine.p3);
+	msg->StartLine.p3 = strdup(tmp);
+
+	free(tmp);
+	return 1;
+}
+
+static int parseHeaderLine (HttpMessage *msg)
+{
+	// using ":" to split key and value£¬from msg->pri_data->linedata
+	ParserData *pd = (ParserData *)msg->pri_data;
+	char *key = (char *)malloc(pd->line_datasize+1);
+	char *value = (char *)malloc(pd->line_datasize+1);
+	const char *p = pd->linedata;
+	const char *tail = p+pd->line_datasize;
+	const char *q = p;
+	const char *x;
+	while (p < tail && isspace(*p))
+		p++;
+
+	q = p;
+	while (p < tail && *p != ':') p++;
+	x = p+1;	// *x = ':'
+
+	p--;
+	while (p > q && isspace(*p)) p--;
+	if (q == p) {
+	}
+	else {
+		memcpy(key, q, p-q+1);
+		key[p-q+1] = 0;
+	}
+
+	q = x;
+
+	p = tail - 1;
+	while (p > q && isspace(*p)) p--;
+	while (*q && isspace(*q)) q++;
+
+	memcpy(value, q, p-q+1);
+	value[p-q+1] = 0;
+
+	httpc_Message_setValue(msg, key, value);
+
+	free(key);
+	free(value);
+	return 1;
+}
+
+static int appendByte (char c, HttpMessage *msg)
+{
+	int ret = 1;
+	ParserData *pd = (ParserData *)msg->pri_data;
+
+	switch (pd->state) {
+		case HTTP_STARTLINE:
+			_append(c, msg);
+			if (c == '\r') {
+				pd->last_state = HTTP_STARTLINE;
+				pd->state = HTTP_CR;
+			}
+			break;
+
+		case HTTP_HEADERS:
+			_append(c, msg);
+			if (c == '\r') {
+				pd->last_state = HTTP_HEADERS;
+				pd->state = HTTP_CR;
+			}
+			break;
+
+		case HTTP_CR:
+			_append(c, msg);
+			if (c == '\n') {
+				pd->state = HTTP_CRLF;
+
+				// blank line ?
+				if (!strncmp(pd->linedata, "\r\n", 2)) {
+					if (_getmsglen(msg) > 0) {
+						pd->state = HTTP_BODY;
+					}
+					else {
+						pd->state = HTTP_COMPLETE;
+					}
+				}
+			}
+			else {
+				pd->state = pd->last_state;
+			}
+			break;
+
+		case HTTP_CRLF:
+			if (isblank(c)) {
+				char *p;
+				// LSW
+				pd->state = HTTP_LSW;
+
+				p = strstr(pd->linedata, "\r\n");
+				if (p) {
+					pd->line_datasize = p-pd->linedata;
+				}
+			}
+			else {
+				if (pd->last_state == HTTP_STARTLINE) {
+					parseStartLine(msg);
+				}
+				else {
+					parseHeaderLine(msg);
+				}
+
+				pd->line_datasize = 0;
+				_append(c, msg);
+
+				if (c == '\r')
+					pd->state = HTTP_CR;
+				else
+					pd->state = HTTP_HEADERS;
+			}
+			break;
+
+		case HTTP_LSW:
+			if (!isblank(c)) {
+				pd->state = pd->last_state;
+				_append(c, msg);
+			}
+			break;
+
+		case HTTP_BODY:
+			msg->body = (char *)realloc(msg->body, msg->bodylen+1);
+			msg->body[msg->bodylen] = c;
+			msg->bodylen++;
+			if (_getmsglen(msg) == msg->bodylen)
+				pd->state = HTTP_COMPLETE;
+			break;
+
+		case HTTP_COMPLETE:
+			break;
+	}
+
+	return ret;
+}
+
+static int appendBody (const char *p, int len, HttpMessage *msg)
+{
+	msg->body = (char *)realloc(msg->body, msg->bodylen+len);
+	memcpy(&msg->body[msg->bodylen], p, len);
+	msg->bodylen += len;
+
+	if (_getmsglen(msg) == msg->bodylen) {
+		ParserData *pd = (ParserData *)msg->pri_data;
+		pd->state = HTTP_COMPLETE;
+	}
+
+	return len;
+}
+
+HttpMessage *httpc_parser_parse (HttpMessage *saved, 
+		const char *data, int len, int *used)
+{
+	ParserData *pd;
+	HttpMessage *msg = saved;
+	const char *p = data;
+
+	*used = 0;
+
+	if (!msg) {
+		msg = httpc_Message_create();
+		msg->pri_data = malloc(sizeof(ParserData));
+		pd = (ParserData *)msg->pri_data;
+		pd->linedata = 0;
+		pd->line_bufsize = 0;
+		pd->line_datasize = 0;
+		pd->state = HTTP_STARTLINE;
+	}
+	else {
+		pd = (ParserData *)msg->pri_data;
+	}
+
+	while (pd->state != HTTP_COMPLETE && len > 0) {
+		if (pd->state == HTTP_BODY) {
+			const char *v = 0;
+			int rc;
+			int bodylen, expect;
+
+			httpc_Message_getValue(msg, "Content-Length", &v);
+			bodylen = atoi(v);
+			expect = bodylen - msg->bodylen;
+			rc = appendBody(p, len > expect ? expect : len, msg);
+			p += rc;
+			len -= rc;
+			*used += rc;
+		}
+		else {
+			int rc = appendByte(*p, msg);
+			p += rc;
+			len -= rc;
+			*used += rc;
+		}
+	}
+
+	return msg;
+}
+
+enum HttpParserState httpc_Message_state (HttpMessage *msg)
+{
+	if (msg && msg->pri_data) {
+		ParserData *pd = (ParserData *)msg->pri_data;
+		return pd->state;
+	}
+	else
+		return HTTP_UNKNOWN;
+}
+
